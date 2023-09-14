686f811d310a51d316d49f3b64173414011e5aab
holgihe <holgihe@gmx.net>
2023-09-14 07:50:36 +0200

    Update Hello_World_hh.cpp

--- Hello_World_hh.cpp
+++ Hello_World_hh.cpp
@@ -3,6 +3,6 @@
 int main()
 {
    std::cout << "Hello World changed now again,puhuhuh.neue--ingithub hinzu. hinzu2. hinzulinux" << std::endl;
-   std::cout << "hallo neu hinzu um 07:26 Samstag !! hinzumitlinux" << std::endl;
+   std::cout << "hallo neu hinzu um 07:26 Samstag !! hinzumitlinux. neu am 14 Sept" << std::endl;
    return 0;
 }
