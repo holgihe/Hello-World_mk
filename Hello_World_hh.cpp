c151e72a67f43c6cf1e3676b0104c5a3caab20d4
holgihe <holgihe@eh-herrgen.com>
2024-08-08 21:52:07 +0200

    Tabs, space were replaced by tab 4wide

--- Hello_World_hh.cpp
+++ Hello_World_hh.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-   std::cout << "Hello World changed now again,puhuhuh.neue--ingithub hinzu. hinzu2. hinzulinux" << std::endl;
-   std::cout << "hallo neu hinzu um 07:26 Samstag !! hinzumitlinux. neu am 14 Sept" << std::endl;
-   // new comment done on laptop - emacs
-   return 0;
+	std::cout << "Hello World changed now again,puhuhuh.neue--ingithub hinzu. hinzu2. hinzulinux" << std::endl;
+	std::cout << "hallo neu hinzu um 07:26 Samstag !! hinzumitlinux. neu am 14 Sept" << std::endl;
+	// new comment done on laptop - emacs
+	return 0;
 }
--- makefile
+++ makefile
@@ -1,11 +1,18 @@
 # This is the default target, which will be built when
 # you invoke make....
 .PHONY: all
-all: hello_world_hh
+all: Hello_World_hh	excercise1	test2
 
 # This rule tells make how to build hello from hello.cpp
-hello_world_hh: Hello_World_hh.cpp
+Hello_World_hh:	Hello_World_hh.cpp
 	g++ -o hello_world_hh  Hello_World_hh.cpp
+
+excercise1:		excercise1.cpp
+	g++ -o excercise1 excercise1.cpp
+
+test2:			test2.cpp
+	g++ -o test2 test2.cpp
+
 
 # This rule tells make to copy hello to the binaries subdirectory,
 # creating it if necessary
