ad6847bab7d49b0e68e91879568d071d4bbc3d39
holgihe <holgihe@eh-herrgen.com>
2024-08-08 22:49:14 +0200

    dddddddd

--- Hello_World_hh.cpp
+++ Hello_World_hh.cpp
@@ -1,9 +1,6 @@
 //New comment done on githug - 02.Aug. 21:12, 21:25
-<<<<<<< HEAD
 /*	This is added on laptop emacs on 08.Aug. 22:27 */
-=======
 /*	Added on github - 08.Aug. 22:24  */
->>>>>>> 62f343dcec9840c7c90e8546e3f7ee0d6ead5c59
 #include <iostream>
 
 int main()
